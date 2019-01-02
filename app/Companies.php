42633d3160f459e2d00ebf2717885cd6e909f07f
Damian Pokorski <damianpokorski@hotmail.com>
2019-01-02 00:32:38 +0000

    Company model changes - Added extra functions for storing and displaying uploaded logos as base64

--- app/Companies.php
+++ app/Companies.php
@@ -7,7 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 class Companies extends Model
 {
     //
-    
+    protected $primaryKey = 'id';
     protected $table = 'companies';
-    
+    protected $fillable = ['name', 'email', 'website'];
+
+    public function save_logo($image_path, $mime_type) {
+        $this->logo = json_encode(
+            [
+                'content' => base64_encode(file_get_contents($image_path)), 
+                'mime' => $mime_type
+            ]
+        );
+    }
+
+    public function get_logo() {
+        return json_decode($this->logo)->content;
+    }
+
+    public function get_logo_mime() {
+        return json_decode($this->logo)->mime;
+    }
+
+    public function logo_as_base_64_css() {
+        return 'data:'.$this->get_logo_mime().';base64,'.$this->get_logo();
+    }
 }
