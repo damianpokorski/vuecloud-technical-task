76cf59295d5502cca2c8d5dd02c9c0d12864f081
Damian Pokorski <damianpokorski@hotmail.com>
2019-01-02 02:17:57 +0000

    Minor whitespace refactor.

--- app/Companies.php
+++ app/Companies.php
@@ -11,7 +11,8 @@ class Companies extends Model
     protected $table = 'companies';
     protected $fillable = ['name', 'email', 'website'];
 
-    public function save_logo($image_path, $mime_type) {
+    public function save_logo($image_path, $mime_type)
+    {
         $this->logo = json_encode(
             [
                 'content' => base64_encode(file_get_contents($image_path)), 
@@ -20,15 +21,23 @@ class Companies extends Model
         );
     }
 
-    public function get_logo() {
+    public function get_logo()
+    {
         return json_decode($this->logo)->content;
     }
 
-    public function get_logo_mime() {
+    public function get_logo_mime()
+    {
         return json_decode($this->logo)->mime;
     }
 
-    public function logo_as_base_64_css() {
+    public function logo_as_base_64_css()
+    {
         return 'data:'.$this->get_logo_mime().';base64,'.$this->get_logo();
+    }
+
+    public function employees()
+    {
+        return $this->hasMany('App\Employees', 'company_id');
     }
 }
